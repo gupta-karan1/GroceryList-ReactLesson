df1fbc429a41a06f676a0527c7720a1b3ead5ee0
Karan Gupta <d22124440@mytudublin.ie>
2023-05-27 12:43:01 +0100

    grocery list

---
+++ src/LineItem.js
@@ -0,0 +1,34 @@
+// Purpose: Component for line item
+import { FaTrashAlt } from "react-icons/fa";
+
+const LineItem = ({ item, handleCheck, handleDelete }) => {
+  return (
+    <li className="item">
+      <input
+        type="checkbox"
+        checked={item.checked}
+        onChange={() => {
+          handleCheck(item.id);
+        }}
+      />
+
+      <label
+        style={item.checked ? { textDecoration: "line-through" } : null}
+        onDoubleClick={() => handleCheck(item.id)}
+      >
+        {item.item}
+      </label>
+
+      <FaTrashAlt
+        onClick={() => {
+          handleDelete(item.id);
+        }}
+        role="button"
+        tabIndex="0"
+        aria-label={`Delete ${item.item}`}
+      />
+    </li>
+  );
+};
+
+export default LineItem;
